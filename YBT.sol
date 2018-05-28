07c459a538fbf33fef3e6882b543cc111362dfd8
ByteEater <noreply>
2018-05-28 03:57:50 +0200

    ERC20 + burning

---
+++ YBT.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.24;
+contract YellowBetterToken {
+    string public constant name = "Yellow Better";
+    string public constant symbol = "YBT";
+    uint8 public constant decimals = 18;
+    uint public constant _totalSupply = 2000000000000000000000000000;
+    uint public totalSupply = _totalSupply;
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+    event Transfer(address indexed, address indexed, uint);
+    event Approval(address indexed, address indexed, uint);
+    event Burn(address indexed, uint);
+    constructor(address creator)
+    {
+        balances[creator] = totalSupply;
+    }
+    function sub(uint a, uint b) private pure returns (uint)
+    {
+        require(a >= b);
+        return a - b;
+    }
+    function balanceOf(address tokenOwner) view returns (uint)
+    {
+        return balances[tokenOwner];
+    }
+    function transfer(address to, uint tokens) returns (bool)
+    {
+        balances[msg.sender] = sub(balances[msg.sender], tokens);
+        balances[to] += tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+    function transferFrom(address from, address to, uint tokens) returns (bool)
+    {
+        balances[from] = sub(balances[from], tokens);
+        allowed[from][msg.sender] = sub(allowed[from][msg.sender], tokens);
+        balances[to] += tokens;
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+    function approve(address spender, uint tokens) returns (bool)
+    {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+    function allowance(address tokenOwner, address spender) view returns (uint)
+    {
+        return allowed[tokenOwner][spender];
+    }
+    function burn(address tokenOwner, uint tokens)
+    {
+        balances[tokenOwner] = sub(balances[tokenOwner], tokens);
+        totalSupply -= tokens;
+        emit Burn(tokenOwner, tokens);
+    }
+}
